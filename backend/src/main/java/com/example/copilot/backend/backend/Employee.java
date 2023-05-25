8a4609b73c7b7b53e9e2a33c17cc8cc1bec4734f
Sebastian.Janczewski-Wickleder <sebastian.janczewskiwickleder@dkb.de>
2023-05-25 11:29:06 +0200

    Employee Entity

---
+++ backend/src/main/java/com/example/copilot/backend/backend/Employee.java
@@ -0,0 +1,24 @@
+package com.example.copilot.backend.backend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table
+public class Employee {
+
+  @Id
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String team;
+}
